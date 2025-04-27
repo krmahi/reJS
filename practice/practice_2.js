df836d2be3c2b6807717fe86ca490ab4bfb17a5c
krmahi <mahesh.kr.2277@gmail.com>
2025-04-27 23:03:07 +0530

    practice: object (product, profile)

---
+++ practice/practice_2.js
@@ -0,0 +1,15 @@
+const profile = {
+  username: "@mahi",
+  isFollow: true,
+  post: 195,
+  followers: 569 + "k",
+  following: 4,
+  shortName: "mahi",
+  threads: "@mahi",
+  fadeText: "Artist",
+  Bio: "blah blah",
+};
+
+console.log(profile);
+console.log(typeof profile);
+console.log(typeof profile.username);
