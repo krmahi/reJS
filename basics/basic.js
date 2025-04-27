a1128d2226b6a7207c367355924b2f2a01e65d50
krmahi <mahesh.kr.2277@gmail.com>
2025-04-27 23:02:22 +0530

    add: main.htm for display of console logs

---
+++ basics/basic.js
@@ -0,0 +1,30 @@
+// console.log("hi");
+// alert("hi");
+name = "mahi";
+/* The line `x = null` is assigning the value `null` to the variable `x`. In JavaScript, `null` is a
+special value that represents the intentional absence of any object value. By assigning `null` to a
+variable, you are explicitly stating that the variable does not currently point to any object or
+value. */
+null_ex = null;
+/* The line `y = undefined` is assigning the value `undefined` to the variable `y`. In JavaScript,
+`undefined` is a primitive value automatically assigned to variables that have not been initialized
+or do not have a value assigned to them. By explicitly assigning `undefined` to a variable, you are
+setting its value to be undefined. */
+var undefine_ex = undefined;
+/* The line `isFollow = true; //camel case` is assigning the boolean value `true` to the variable
+`isFollow`. The comment `//camel case` is providing a note or reminder that the variable `isFollow`
+is named using camel case convention, which is a naming convention where multiple words are joined
+together and each word, except the first one, starts with a capital letter. */
+isFollow = true; //camel case
+
+console.log(isFollow);
+
+console.log(null_ex);
+console.log(undefine_ex);
+
+/* The line `name = 2` is reassigning the variable `name` with the value `2`. Initially, the variable
+`name` was assigned the string value `"mahi"`, but with this line of code, it is being overwritten
+with the numeric value `2`. JavaScript is a dynamically typed language, so variables can change
+their data type during the execution of the program. */
+name = 2;
+console.log(name);
